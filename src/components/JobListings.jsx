4fd21980fb470be8614d10a376450daf77766b0d
baokimho <kim.ho@edu.turkuamk.fi>
2025-05-06 09:47:08 +0300

    fetching api to get the job list

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import jobs from '../jobs.json'
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing.jsx'
 
 const JobListings = ( {isHome = false}) => {
-  const jobListings = isHome ?  jobs.slice(0,3) : jobs
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
 
-  return (
+  useEffect( () =>{
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs')
+        const data = await res.json()
+        setJobs(data)        
+      } catch (error) {
+        console.log('Error fetching jobs:', error)
+      } finally {
+        setLoading(false)
+        console.log('Jobs fetching completed')
+      }
+
+    }
+
+  fetchJobs()
+  }, [])
+  return ( 
     <>
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -13,7 +31,7 @@ const JobListings = ( {isHome = false}) => {
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobListings.map((job) => (
+            {jobs.map((job) => (
               <JobListing key={ job.id } job= { job }/>
             ))}
         </div>
